3252f934e536f8d164ee4f8ea33b6ce6f9ccc2ed
Arnaud TANGUY <arn.tanguy@gmail.com>
2022-10-30 23:24:53 -0700

    Adapt to new Robot loaders

--- src/ObjectObserver.cpp
+++ src/ObjectObserver.cpp
@@ -40,7 +40,7 @@ void ObjectObserver::configure(const mc_control::MCController & controller, cons
     topic_ = static_cast<std::string>(config("Object")("topic"));
     isInRobotMap_ = config("Object")("inRobotMap", false);
 
-    robots_ = mc_rbdyn::loadRobot(ctl.robot(object_).module(), object_);
+    robots_ = mc_rbdyn::loadRobot(object_, ctl.robot(object_).module());
   }
   else
   {
